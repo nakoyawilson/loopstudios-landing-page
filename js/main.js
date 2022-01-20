556803cf8513cdd738ffb156b706ec76215da0b3
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-01-20 09:52:10 -0400

    Make adjustments to positioning on mobile

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const menuButton = document.querySelector("button");
+const menuButton = document.querySelector(".menu-button");
 const navigation = document.querySelector(".navigation");
 const headerLogo = document.querySelector(".header-logo");
 const mobileMenuContainer = document.querySelector(".mobile");
 menuButton.addEventListener("click", () => {
   navigation.classList.toggle("open");
-  headerLogo.classList.toggle(".mobile-menu");
-  mobileMenuContainer.classList.toggle(".mobile-menu");
+  headerLogo.classList.toggle("mobile-menu");
+  mobileMenuContainer.classList.toggle("mobile-menu");
   if (navigation.classList.contains("open")) {
     menuButton.innerHTML = `<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z" fill="#FFF" fill-rule="evenodd"/></svg>`;
   } else {
